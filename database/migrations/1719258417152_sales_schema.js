4f962ff378487f7c32a0daa2ea578be2533fb67f
juan-formoso <trajano.juan@gmail.com>
2024-06-24 17:36:32 -0400

    Atualizando sales_schema.js; Definição da estrutura de dados da migration.

--- database/migrations/1719258417152_sales_schema.js
+++ database/migrations/1719258417152_sales_schema.js
@@ -7,6 +7,14 @@ class SalesSchema extends Schema {
   up () {
     this.table('sales', (table) => {
       // alter table
+      table.increments()
+      table.integer('client_id').unsigned().references('id').inTable('clients')
+      table.integer('product_id').unsigned().references('id').inTable('products')
+      table.integer('quantity').notNullable()
+      table.decimal('unit_price', 12, 2).notNullable()
+      table.decimal('total_price', 12, 2).notNullable()
+      table.datetime('sale_date').notNullable()
+      table.timestamps()
     })
   }
 
