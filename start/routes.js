8860359bba7a0fe4c21bcaeef3ebb54f5370e0ba
juan-formoso <trajano.juan@gmail.com>
2024-06-24 22:50:27 -0400

    Implementando rotas em start/routes.js

--- start/routes.js
+++ start/routes.js
@@ -1,21 +1,27 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URLs and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
-|
-*/
-
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-})
+// Rotas de autenticação
+Route.post('/signup', 'UserController.signup')
+Route.post('/login', 'UserController.login')
+
+Route.group(() => {
+  // Rotas de Clientes
+  Route.get('/clients', 'ClientController.index')
+  Route.get('/clients/:id', 'ClientController.show')
+  Route.post('/clients', 'ClientController.store')
+  Route.put('/clients/:id', 'ClientController.update')
+  Route.delete('/clients/:id', 'ClientController.delete')
+
+  // Rotas de Produtos
+  Route.get('/products', 'Product.Controller.index')
+  Route.get('/products/:id', 'ProductController.show')
+  Route.post('/products', 'ProductController.store')
+  Route.put('/products/:id', 'ProductController.update')
+  Route.delete('/products/:id', 'ProductController.delete')
+
+  // Rotas de Vendas
+  Route.post('/sales', 'SaleController.store')
+}).middleware(['auth']) // Aplica o middleware 'auth' a todas as rotas dentro do grupo
