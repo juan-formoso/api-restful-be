878c2a34d8e444a52c6b7d31ecca7fc95e8fe709
juan-formoso <trajano.juan@gmail.com>
2024-06-25 21:12:35 -0400

    Atualiza Models/Client.js; Inicializa classe e adiciona relacionamentos.

--- app/Models/Client.js
+++ app/Models/Client.js
@@ -4,6 +4,25 @@
 const Model = use('Model')
 
 class Client extends Model {
+  static boot () {
+    super.boot()
+  }
+
+  // Relacionamento 1:N entre Client e Address
+  addresses () {
+    return this.hasMany('App/Models/Address')
+  }
+
+  // Relacionamento 1:N entre Client e Phone
+  phones () {
+    return this.hasMany('App/Models/Phone')
+  }
+
+  // Relacionamento 1:N entre Client e Sale
+  sales () {
+    return this.hasMany('App/Models/Sale')
+  }
 }
 
 module.exports = Client
+
